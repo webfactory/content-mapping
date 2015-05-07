9ceb03f073aedbdbdcd4e4520467c995d2331383
Malte Wunsch <mw@webfactory.de>
2015-05-07 15:21:44 -0200

    Beginn Implementierung Synchronizer (Case 35392)

--- Synchronizer.php
+++ Synchronizer.php
@@ -17,6 +17,11 @@ final class Synchronizer
     private $source;
 
     /**
+     * @var \Iterator
+     */
+    private $sourceQueue;
+
+    /**
      * @var Mapper
      */
     private $mapper;
@@ -27,9 +32,19 @@ final class Synchronizer
     private $destination;
 
     /**
+     * @var \Iterator
+     */
+    private $destinationQueue;
+
+    /**
      * @var LoggerInterface
      */
     private $logger;
+
+    /**
+     * @var string value of the synchronize() parameter
+     */
+    private $className;
 
     /**
      * @param SourceAdapter $source
@@ -56,5 +71,72 @@ final class Synchronizer
      */
     public function synchronize($className)
     {
+        $this->className = $className;
+        $this->sourceQueue = $this->source->getObjectsOrderedById();
+        $this->destinationQueue = $this->destination->getObjectsOrderedById($className);
+
+        while ($this->sourceQueue->valid() && $this->destinationQueue->valid()) {
+            $this->compareQueuesAndReactAccordingly();
+        }
+
+        $this->insertRemainingSourceObjects();
+        $this->deleteRemainingDestinationObjects();
+
+        $this->destination->commit();
+    }
+
+    private function compareQueuesAndReactAccordingly()
+    {
+        $sourceObject = $this->sourceQueue->current();
+        $sourceObjectId = $this->mapper->idOf($sourceObject);
+        $destinationObject = $this->destinationQueue->current();
+        $destinationObjectId = $this->destination->idOf($destinationObject);
+
+        if ($destinationObjectId > $sourceObjectId) {
+            $this->insert($sourceObject);
+        } elseif ($destinationObjectId < $sourceObjectId) {
+            $this->delete($destinationObject);
+        } elseif ($destinationObjectId === $sourceObjectId) {
+            // if mapper->map() === true -> updated
+        } else {
+            $this->destinationQueue->next();
+            $this->sourceQueue->next();
+        }
+    }
+
+    /**
+     * @param mixed $sourceObject
+     */
+    private function insert($sourceObject)
+    {
+        $newObjectInDestinationSystem = $this->destination->createObject(
+            $this->mapper->idOf($sourceObject),
+            $this->className
+        );
+        $this->mapper->map($sourceObject, $newObjectInDestinationSystem);
+
+        $this->sourceQueue->next();
+    }
+
+    /**
+     * @param mixed $destinationObject
+     */
+    private function delete($destinationObject)
+    {
+        $this->destinationQueue->next();
+    }
+
+    private function insertRemainingSourceObjects()
+    {
+        while ($this->sourceQueue->valid()) {
+            $this->insert($this->sourceQueue->current());
+        }
+    }
+
+    private function deleteRemainingDestinationObjects()
+    {
+        while ($this->destinationQueue->valid()) {
+            $this->delete($this->destinationQueue->current());
+        }
     }
 }
