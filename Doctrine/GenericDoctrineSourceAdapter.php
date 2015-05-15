ce420f774e4d0b67f0060748a4cf2e0677a10129
Malte Wunsch <mw@webfactory.de>
2015-05-15 06:06:16 -0200

    GenericDoctrineSourceAdapter zugefuegt (Case 35392)

---
+++ Doctrine/GenericDoctrineSourceAdapter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Webfactory\ContentMapping\Doctrine;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityRepository;
+use Webfactory\ContentMapping\SourceAdapter;
+
+/**
+ * Implementation for Doctrine as a source system.
+ *
+ * @final by default.
+ */
+final class GenericDoctrineSourceAdapter implements SourceAdapter
+{
+    /**
+     * @var EntityRepository
+     */
+    private $repository;
+
+    /**
+     * @var string
+     */
+    private $repositoryMethod;
+
+    /**
+     * @param EntityRepository $repository to query
+     * @param string $repositoryMethod Which returns a Collection of all objects to map, ordered by their ascending IDs.
+     */
+    public function __construct($repository, $repositoryMethod)
+    {
+        $this->repository = $repository;
+        $this->repositoryMethod = $repositoryMethod;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getObjectsOrderedById()
+    {
+        $entities = $this->repository->{$this->repositoryMethod}();
+        if (is_array($entities) === false) {
+            throw new \RuntimeException(
+                'The result of ' . get_class($this->repository) . '->' . $this->repositoryMethod . '() is no array, '
+                . 'which it has to be if you wish to use ' . __CLASS__
+            );
+        }
+
+        /** @var Collection mixed[] */
+        return new \ArrayIterator($entities);
+    }
+}
